6dd2b1c08e3e9fc2b526e2ef3f43449f10658fd1
Eien <oleksiaksebastian32@gmail.com>
2024-07-18 20:20:35 +0200

    Added header profile image and created a special folder for it, and displayed on the page

--- Scripts/Render/renderHeader.js
+++ Scripts/Render/renderHeader.js
@@ -1,4 +1,28 @@
 function renderHeader() {
+  const header = document.querySelector(".js-header");
+
+  header.innerHTML += `
+    <div class="header-left">
+      <div class="profile">
+        <a href="https://github.com/Brzdac3er" class="profile-link">
+          <img src="../../Images/Header-Profile/header-profile.png" alt="profile picture" 
+            class="profile-picture" />
+        </a> 
+      </div>
+    </div>
+
+    <div class="header-middle">
+      <div class="creator">
+        <p class="creator-name">Created by: Eien</p>
+      </div>
+    </div>
+
+    <div class="header-right">
+      <div class="other-projects">
+        <a href="" class="other-project-link">Other Projects</a>
+      </div>
+    </div>
+  `;
 }
 
 export default renderHeader;
