228394ef99fdb88ce8f1974d7aaef15625e3cbf0
Eien <oleksiaksebastian32@gmail.com>
2024-07-19 16:52:58 +0200

    Deleted renderFooter.js

--- Scripts/Render/renderFooter.js
+++
@@ -1,5 +0,0 @@
-function renderFooter() {
-
-}
-
-export default renderFooter;
--- Scripts/calculator.js
+++ Scripts/calculator.js
@@ -1,7 +1,5 @@
 import renderHeader from "./Render/renderHeader.js";
 import renderCalculator from "./Render/renderCalculator.js";
-import renderFooter from "./Render/renderFooter.js";
 
 renderHeader();
 renderCalculator();
-renderFooter();
