01822dffd1e6e084b643b1fd66fe84c36ec4da32
Eien <oleksiaksebastian32@gmail.com>
2024-07-20 19:10:14 +0200

    Added var.css, created some variables and added new classes in renderCalculator.js

--- Scripts/Render/renderCalculator.js
+++ Scripts/Render/renderCalculator.js
@@ -3,42 +3,43 @@ function renderCalculator() {
   calculatorHTML.innerHTML += `
     <div class="calculator-body">
       <div class="calculation-display">
+        <input type="text" name="display" class="input-display"/>
       </div>
 
       <div class="calculator-btns">
         <div class="top-btns">
-          <button class="top-btn">Clear</button>
-          <button class="top-btn">√</button>
-          <button class="top-btn">/</button>
-          <button class="top-btn">X</button>
+          <button class="top-btn secondary-btn">Clear</button>
+          <button class="top-btn secondary-btn">√</button>
+          <button class="top-btn secondary-btn">/</button>
+          <button class="top-btn secondary-btn">X</button>
         </div>
         
         <div class="semi-top-btns">
-          <button class="semi-top-btn">7</button>
-          <button class="semi-top-btn">8</button>
-          <button class="semi-top-btn">9</button>
-          <button class="semi-top-btn">-</button>
+          <button class="semi-top-btn primary-btn">7</button>
+          <button class="semi-top-btn primary-btn">8</button>
+          <button class="semi-top-btn primary-btn">9</button>
+          <button class="semi-top-btn secondary-btn">-</button>
         </div>
 
         <div class="mid-btns">
-          <button class="mid-btn">4</button>
-          <button class="mid-btn">5</button>
-          <button class="mid-btn">6</button>
-          <button class="mid-btn">+</button>
+          <button class="mid-btn primary-btn">4</button>
+          <button class="mid-btn primary-btn">5</button>
+          <button class="mid-btn primary-btn">6</button>
+          <button class="mid-btn secondary-btn">+</button>
         </div>
 
         <div class="semi-mid-btns">
-          <button class="semi-mid-btn">1</button>
-          <button class="semi-mid-btn">2</button>
-          <button class="semi-mid-btn">3</button>
-          <button class="semi-mid-btn">+/-</button>
+          <button class="semi-mid-btn primary-btn">1</button>
+          <button class="semi-mid-btn primary-btn">2</button>
+          <button class="semi-mid-btn primary-btn">3</button>
+          <button class="semi-mid-btn secondary-btn">+/-</button>
         </div>
 
         <div class="bot-btns">
-          <button class="bot-btn">0</button>
-          <button class="bot-btn">.</button>
-          <button class="bot-btn">=</button>
-          <button class="bot-btn">%</button>
+          <button class="bot-btn primary-btn">0</button>
+          <button class="bot-btn primary-btn">.</button>
+          <button class="bot-btn primary-btn">=</button>
+          <button class="bot-btn secondary-btn">%</button>
         </div>
       </div>
     </div>
