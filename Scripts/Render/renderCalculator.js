c6043292ab1d949c632c72cead86fe930ebe9e1d
Eien <oleksiaksebastian32@gmail.com>
2024-07-22 12:01:06 +0200

    Make interactive all primary buttons and secondary buttons beside show history, deleted percent and root buttons and changed layout and added saving to loacal storage, clear all button, show history button

--- Scripts/Render/renderCalculator.js
+++ Scripts/Render/renderCalculator.js
@@ -3,45 +3,48 @@ export function renderCalculator() {
   calculatorHTML.innerHTML += `
     <div class="calculator-body">
       <div class="calculation-display">
-        <input type="text" name="display" placeholder="0" class="input-display"/>
+        <input type="text" name="display" class="input-display js-input-display"/>
       </div>
 
       <div class="calculator-btns">
         <div class="top-btns">
-          <button class="top-btn secondary-btn clear-btn">Clear</button>
-          <button class="top-btn secondary-btn">√</button>
-          <button class="top-btn secondary-btn">/</button>
-          <button class="top-btn secondary-btn">X</button>
+          <button class="top-btn secondary-btn clear-btn js-clear-btn">Clear</button>
+          <button class="semi-mid-btn secondary-btn js-clear-all-btn">CA</button>
+          <button class="bot-btn secondary-btn js-show-history-btn">SH</button>
         </div>
         
         <div class="semi-top-btns">
-          <button class="semi-top-btn primary-btn">7</button>
-          <button class="semi-top-btn primary-btn">8</button>
-          <button class="semi-top-btn primary-btn">9</button>
-          <button class="semi-top-btn secondary-btn">-</button>
+          <button class="semi-top-btn primary-btn js-seven-btn">7</button>
+          <button class="semi-top-btn primary-btn js-eight-btn">8</button>
+          <button class="semi-top-btn primary-btn js-nine-btn">9</button>
+          <button class="semi-top-btn secondary-btn js-minus-btn">-</button>
         </div>
 
         <div class="mid-btns">
-          <button class="mid-btn primary-btn">4</button>
-          <button class="mid-btn primary-btn">5</button>
-          <button class="mid-btn primary-btn">6</button>
-          <button class="mid-btn secondary-btn">+</button>
+          <button class="mid-btn primary-btn js-four-btn">4</button>
+          <button class="mid-btn primary-btn js-five-btn">5</button>
+          <button class="mid-btn primary-btn js-six-btn">6</button>
+          <button class="mid-btn secondary-btn js-plus-btn">+</button>
         </div>
 
         <div class="semi-mid-btns">
-          <button class="semi-mid-btn primary-btn">1</button>
-          <button class="semi-mid-btn primary-btn">2</button>
-          <button class="semi-mid-btn primary-btn">3</button>
-          <button class="semi-mid-btn secondary-btn">+/-</button>
+          <button class="semi-mid-btn primary-btn js-one-btn">1</button>
+          <button class="semi-mid-btn primary-btn js-two-btn">2</button>
+          <button class="semi-mid-btn primary-btn js-three-btn">3</button>
+          <button class="top-btn secondary-btn js-multiply-btn">X</button>
         </div>
 
         <div class="bot-btns">
-          <button class="bot-btn primary-btn">0</button>
-          <button class="bot-btn primary-btn">.</button>
-          <button class="bot-btn primary-btn result-btn">=</button>
-          <button class="bot-btn secondary-btn">%</button>
+          <button class="bot-btn primary-btn js-zero-btn">0</button>
+          <button class="bot-btn primary-btn js-dot-btn">.</button>
+          <button class="bot-btn primary-btn equal-btn js-equal-btn">=</button>
+          <button class="top-btn secondary-btn js-devide-btn">/</button>
         </div>
       </div>
+    </div>
+
+    <div class="history js-history">
+      <h1 class="history-title">History</h1>
     </div>
   `;
 }
--- Scripts/calculator.js
+++ Scripts/calculator.js
@@ -3,3 +3,105 @@ import { renderCalculator } from "./Render/renderCalculator.js";
 
 renderHeader();
 renderCalculator();
+
+function calculator() {
+  const equalBtn = document.querySelector(".js-equal-btn"); 
+  const clearBtn = document.querySelector(".js-clear-btn"); 
+  const clearAllBtn = document.querySelector(".js-clear-all-btn");
+  const showHistoryBtn = document.querySelector(".js-show-history-btn");
+
+  const nineBtn = document.querySelector(".js-nine-btn");
+  const eightBtn = document.querySelector(".js-eight-btn");
+  const sevenBtn = document.querySelector(".js-seven-btn");
+  const sixBtn = document.querySelector(".js-six-btn");
+  const fiveBtn = document.querySelector(".js-five-btn");
+  const fourBtn = document.querySelector(".js-four-btn");
+  const threeBtn = document.querySelector(".js-three-btn");
+  const twoBtn = document.querySelector(".js-two-btn");
+  const oneBtn = document.querySelector(".js-one-btn");
+  const zeroBtn = document.querySelector(".js-zero-btn");
+
+  const dotBtn = document.querySelector(".js-dot-btn");
+  const devideBtn = document.querySelector(".js-devide-btn");
+  const multiplyBtn = document.querySelector(".js-multiply-btn");
+  const minusBtn = document.querySelector(".js-minus-btn");
+  const plusBtn = document.querySelector(".js-plus-btn");
+
+
+  document.body.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      equal();
+    }
+    if (event.key === "Backspace") {
+      clear();
+    }
+  });
+  equalBtn.addEventListener("click", () => equal());
+  clearBtn.addEventListener("click", () => clear());
+  clearAllBtn.addEventListener("click", () => clearHistory());
+  showHistoryBtn.addEventListener("click", () => showHistory());
+
+  nineBtn.addEventListener("click", () => calculate("9"));
+  eightBtn.addEventListener("click", () => calculate("8"));
+  sevenBtn.addEventListener("click", () => calculate("7"));
+  sixBtn.addEventListener("click", () => calculate("6"));
+  fiveBtn.addEventListener("click", () => calculate("5"));
+  fourBtn.addEventListener("click", () => calculate("4"));
+  threeBtn.addEventListener("click", () => calculate("3"));
+  twoBtn.addEventListener("click", () => calculate("2"));
+  oneBtn.addEventListener("click", () => calculate("1"));
+  zeroBtn.addEventListener("click", () => calculate("0"));
+
+  dotBtn.addEventListener("click", () => calculate("."));
+  devideBtn.addEventListener("click", () => calculate(" / "));
+  multiplyBtn.addEventListener("click", () => calculate(" * "));
+  minusBtn.addEventListener("click", () => calculate(" - "));
+  plusBtn.addEventListener("click", () => calculate(" + "));
+}
+
+function calculate(sign) {
+  const calculationDisplay = document.querySelector(".js-input-display");
+  let calculation = sign;
+
+  calculationDisplay.value += calculation;
+  saveToStorage();
+}
+
+function clear() {
+  const calculationDisplay = document.querySelector(".js-input-display");
+
+  calculationDisplay.value = "";
+}
+
+function equal() {
+  const calculationDisplay = document.querySelector(".js-input-display");
+
+  calculationDisplay.value = eval(calculationDisplay.value);
+  saveToStorage();
+}
+
+function clearHistory() {
+  const calculationDisplay = document.querySelector(".js-input-display");
+  calculationDisplay.value = "";
+
+  localStorage.removeItem("calculation");
+  saveToStorage();
+}
+
+function saveToStorage() {
+  const calculationDisplay = document.querySelector(".js-input-display");
+  localStorage.setItem("calculation", calculationDisplay.value);
+}
+
+function getFromStorage() {
+  const calculationDisplay = document.querySelector(".js-input-display");
+  calculationDisplay.value = localStorage.getItem("calculation"); 
+}
+
+function showHistory() {
+  const historyContainer = document.querySelector(".js-history");
+  historyContainer.classList.add("show");
+}
+
+getFromStorage();
+calculator();
